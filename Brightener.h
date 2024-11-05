f81ec95959a4a43986124162088d85a54feca5c9
Ponniah <ShunmugaSundaram.P@philips.com>
2024-11-05 15:25:09 +0530

    Fix compiler error

--- Brightener.h
+++ Brightener.h
@@ -1,12 +1,6 @@
 #pragma once
 #include <cstdint>
 
-struct Image {
-	int rows;
-	int columns;
-	uint8_t pixels[1024 * 1024]; // max 1k x 1k image
-};
-
 class ImageBrightener {
 private:
 	Image m_inputImage;
